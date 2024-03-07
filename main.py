e735af51a5ee77af8d017ad070e898f9d9a75688
Probando73 <153937042+Probando73@users.noreply.github.com>
2024-03-07 08:28:01 -0300

    Update main.py
    Se adaptaron las funciones a POO

--- main.py
+++ main.py
@@ -1,89 +1,124 @@
 ############################################# Primera instancia #############################################
 import pandas as pd
 
-##### Funciones #####
+##### Clases #####
 
-    def calcular_ajuste_inflacion(alquiler, **iterador):
-        monto_alquiler = alquiler
-        contador_mes = 0
-        monto_ajustado = {}
-        
-        for k, v in iterador.items():
-        valor_ajustado = monto_alquiler * (1 + (v / 100))
-        monto_alquiler = round(valor_ajustado, 2)
-        monto_ajustado[meses_alquiler[contador_mes]] = monto_alquiler
+class Ajuste_Inflacion():
+  """ 
+  Clase para poder calcular el ajuste por IPC y REM 
+  """
+
+  def __init__(self, alquiler):
+      """
+      Metodo constructor que inicia la clase y declara la variable de instancia
+      """
+      self.monto_alquiler = alquiler
+
+
+  def calcular_ajuste_inflacion(self, **iterador):
+      """
+      Metodo para realizar el ajuste por inflacion
+      """
+
+      contador_mes = 0
+      monto_ajustado = {}
+
+      for k, v in iterador.items():
+        valor_ajustado = self.monto_alquiler * (1 + (v / 100))
+        self.monto_alquiler = round(valor_ajustado, 2)
+        monto_ajustado[meses_alquiler[contador_mes]] = self.monto_alquiler
         contador_mes += 1
-        
-        return monto_ajustado
+
+      return monto_ajustado
+
+
+
+class Ajuste_Mep():
+    """ 
+    Clase para poder calcular el ajuste por IPC y REM 
+    """
+
+    def __init__(self):
+      """
+      Metodo constructor que inicia la clase y declara la variable de instancia
+      """
+    pass
     
     
-    def calcular_ajuste_usd(variable_rem, **iterador):
+    def calcular_ajuste_usd(self, variable_rem, **iterador):
+      """
+      Metodo para realizar el ajuste por inflacion
+      """
     
-        contador_mes = 0
-        monto_ajustado = {}
-        lista = []
-        
-        for x, y in variable_rem.items():
+      contador_mes = 0
+      monto_ajustado = {}
+      lista = []
+    
+      for x, y in variable_rem.items():
         lista.append(y)
-        
-        for k, v in iterador.items():
+    
+      for k, v in iterador.items():
         valor_ajustado = int(lista[contador_mes]) / (v)
         monto_alquiler = round(valor_ajustado, 2)
         monto_ajustado[meses_alquiler[contador_mes]] = monto_alquiler
         contador_mes += 1
-        
-        return monto_ajustado
     
-    
-    if __name__ == '__main__':
-        # Datos del programa #
-        ipc = {'Marzo': 0, 'Abril': 0, 'Mayo': 0, 'Junio': 0, 'Julio': 0}
-        
-        rem = {'Marzo': 15.3, 'Abril': 13, 'Mayo': 10, 'Junio': 8.2, 'Julio': 8}
-        
-        mep = {
-          'Abril': 1053,
-          'Mayo': 1074.06,
-          'Junio': 1095.04,
-          'Julio': 1117.45,
-          'Agosto': 1139.80
-        }
-        
-        alquiler = 280000
-        meses = [ipc.keys]
-        datos = [rem.values(), ipc.values(), mep.values()]
-        meses = rem.keys()
-        indice = ['Rem (%)', 'IPC (%)', 'MEP ($)']
-        tabla = pd.DataFrame(data=datos, index=indice, columns=meses)
-        
-        print(tabla)
-        
-        #### Tabla variaciones ###
-        
-        meses_alquiler = []
-        
-        for x in meses:  # Se separan los meses de la tupla.
-        meses_alquiler.append(x)
-        
-        meses_alquiler.append('Agosto')
-        meses_alquiler.remove('Marzo')
+      return monto_ajustado
 
-        # Obtencion de valores ajustados
-        alquiler_rem = calcular_ajuste_inflacion(alquiler, **rem)
-        alquiler_ipc = calcular_ajuste_inflacion(alquiler, **ipc)
-        alquiler_rem_mep = calcular_ajuste_usd(alquiler_rem, **mep)
-        alquiler_rem_ipc = calcular_ajuste_usd(alquiler_ipc, **mep)
-        
-        indice_ajuste = ['Aj REM ($)', 'Aj IPC ($)', 'Aj REM/MEP ($)', 'Aj IPC/MEP ($)']
-        
-        datos_ajustados = [
-          alquiler_rem.values(),
-          alquiler_ipc.values(),
-          alquiler_rem_mep.values(),
-          alquiler_rem_ipc.values(),
-        ]
-        tabla_ajustada = pd.DataFrame(data=datos_ajustados,
-                                    index=indice_ajuste,
-                                    columns=meses_alquiler)
-        
-        print(tabla_ajustada)
+
+if __name__ == '__main__':
+    # Datos del programa #
+    ipc = {'Marzo': 0, 'Abril': 0, 'Mayo': 0, 'Junio': 0, 'Julio': 0}
+
+    rem = {'Marzo': 15.3, 'Abril': 13, 'Mayo': 10, 'Junio': 8.2, 'Julio': 8}
+
+    mep = {
+      'Abril': 1053,
+      'Mayo': 1074.06,
+      'Junio': 1095.04,
+      'Julio': 1117.45,
+      'Agosto': 1139.80
+    }
+
+    alquiler = 280000
+    meses = [ipc.keys]
+    datos = [rem.values(), ipc.values(), mep.values()]
+    meses = rem.keys()
+    indice = ['Rem (%)', 'IPC (%)', 'MEP ($)']
+    tabla = pd.DataFrame(data=datos, index=indice, columns=meses)
+
+    print(tabla)
+
+    #### Tabla variaciones ###
+
+    meses_alquiler = []
+
+    for x in meses:  # Se separan los meses de la tupla.
+      meses_alquiler.append(x)
+
+    meses_alquiler.append('Agosto')
+    meses_alquiler.remove('Marzo')
+
+    # Instancia de clases
+    obj_ajuste_inflacion = Ajuste_Inflacion(alquiler)
+    obj_ajuste_mep = Ajuste_Mep()
+
+    # Obtencion de valores ajustados
+    alquiler_rem = obj_ajuste_inflacion.calcular_ajuste_inflacion(**rem)
+    alquiler_ipc = obj_ajuste_inflacion.calcular_ajuste_inflacion(**ipc)
+    alquiler_rem_mep = obj_ajuste_mep.calcular_ajuste_usd(alquiler_rem, **mep)
+    alquiler_rem_ipc = obj_ajuste_mep.calcular_ajuste_usd(alquiler_ipc, **mep)
+
+    indice_ajuste = ['Aj REM ($)', 'Aj IPC ($)', 'Aj REM/MEP ($)', 'Aj IPC/MEP ($)']
+
+    datos_ajustados = [
+      alquiler_rem.values(),
+      alquiler_ipc.values(),
+      alquiler_rem_mep.values(),
+      alquiler_rem_ipc.values(),
+    ]
+    tabla_ajustada = pd.DataFrame(data=datos_ajustados,
+                                index=indice_ajuste,
+                                columns=meses_alquiler)
+
+    print(tabla_ajustada)
